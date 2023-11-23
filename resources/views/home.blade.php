a6239ad556cfe5e959a69abb36bcf9fd98ac7b6d
NathanEllis1 <nathane@bayshorefin.com>
2023-11-23 12:46:02 -0500

    jumbotron added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,11 @@
 <x-layout>
-    
+    <div class="jumbotron text-center" style=''>
+        <div class=''>
+            <h1>Welcome to Computer's Online</h1><br/>
+            <button class='buttons'>View our Inventory</button>
+        </div><br/>
+        <div class='row'>
+            
+        </div>
+    </div>
 </x-layout>
